0f55bd6205b435b373473c944204481b43c50d41
Aminmenestin <96004789+Aminmenestin@users.noreply.github.com>
2023-08-17 22:38:05 +0330

    dv

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,25 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use App\Http\Resources\brandResource;
+use Illuminate\Support\Facades\Validator;
 
-class BrandController extends Controller
+class BrandController extends ApiController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return 'ok';
     }
 
     /**
@@ -28,21 +24,28 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return 'ok';
+        $validator = Validator::make($request->all() , [
+            'name' => 'required',
+            'display_name' => 'required',
+        ]);
+
+        if($validator->fails()){
+            $this->errorResponse(new brandResource($validator->messages()) , 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'display_name' => $request->display_name,
+        ]);
+
+        return $this->successResponse(new brandResource($user) , 200);
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Brand $brand)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Brand $brand)
     {
         //
     }
